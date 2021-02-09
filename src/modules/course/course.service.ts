332836f17b9e8d104521222550caca9939a29108
Leonardo <oliveirabalsa2@gmail.com>
2021-02-08 22:13:07 -0300

    fix: remove unused comments

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -12,6 +12,7 @@ import { CourseDTO } from './dto/CourseDTO';
 export class CourseService {
   constructor(private http: HttpService) {}
   async findAll(authorization): Promise<CourseDTO[]> {
+    // Implementar modulo HTTP que faz uma chamada para a nossa api passando as credenciais!
     try {
       const { data: course } = await this.http
         .get<CourseDTO[]>(`${BASE_URL}/${V2}/${COURSE}`, {
@@ -25,6 +26,5 @@ export class CourseService {
     } catch (err) {
       throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    // Implementar modulo HTTP que faz uma chamada para a nossa api passando as credenciais!
   }
 }
